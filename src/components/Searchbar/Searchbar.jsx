dd71c597495b6506608f3afed8ae6d7cf952e85a
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-29 21:19:00 +0200

    Searchbar switched to hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import { useState } from 'react';
+// import { Component } from 'react';
 import {
   SearchBarHeader,
   SearchForm,
@@ -10,56 +11,115 @@ import { AiOutlineSearch } from 'react-icons/ai';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { notifyInit } from '../index.styled';
 
-const INITIAL_STATE = {
-  inputQuery: '',
-};
+const INITIAL_STATE = '';
 
-export class Searchbar extends Component {
-  state = INITIAL_STATE;
+export const Searchbar = ({ onSubmit, currentPage: { page, totalPage } }) => {
+  const [value, setValue] = useState(INITIAL_STATE);
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.inputQuery.trim() === '') {
+    if (value.trim() === '') {
       Notify.info(`Please, enter your request`, notifyInit);
       return;
     }
-    this.props.onSubmit(this.state.inputQuery.trim());
-    this.setState(INITIAL_STATE);
+    onSubmit(value.trim());
+    setValue(INITIAL_STATE);
   };
 
-  handleInput = e => {
-    this.setState({
-      inputQuery: e.target.value.toLowerCase(),
-    });
+  const handleInput = ({ target: { value } }) => {
+    setValue(value);
   };
 
-  render() {
-    const {
-      currentPage: { page, totalPage },
-    } = this.props;
-    return (
-      <SearchBarHeader>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchFormBtn type="submit">
-            <AiOutlineSearch />
-          </SearchFormBtn>
-          <SearchFormInput
-            type="text"
-            name="searchQuery"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.handleInput}
-            value={this.state.inputQuery}
-          />
-        </SearchForm>
+  return (
+    <SearchBarHeader>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormBtn type="submit">
+          <AiOutlineSearch />
+        </SearchFormBtn>
+        <SearchFormInput
+          type="text"
+          name="searchQuery"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleInput}
+          value={value}
+        />
+      </SearchForm>
 
-        {totalPage > 1 && (
-          <CurrentPage>
-            {page}/{totalPage}
-          </CurrentPage>
-        )}
-      </SearchBarHeader>
-    );
-  }
-}
+      {totalPage > 1 && (
+        <CurrentPage>
+          {page}/{totalPage}
+        </CurrentPage>
+      )}
+    </SearchBarHeader>
+  );
+};
+
+//* ====== Old code before transitioning to hooks from class components. ======
+
+// import { Component } from 'react';
+// import {
+//   SearchBarHeader,
+//   SearchForm,
+//   SearchFormBtn,
+//   SearchFormInput,
+//   CurrentPage,
+// } from './Searchbar.styled';
+// import { AiOutlineSearch } from 'react-icons/ai';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { notifyInit } from '../index.styled';
+
+// const INITIAL_STATE = {
+//   inputQuery: '',
+// };
+
+// export class Searchbar extends Component {
+//   state = INITIAL_STATE;
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     if (this.state.inputQuery.trim() === '') {
+//       Notify.info(`Please, enter your request`, notifyInit);
+//       return;
+//     }
+//     this.props.onSubmit(this.state.inputQuery.trim());
+//     this.setState(INITIAL_STATE);
+//   };
+
+//   handleInput = e => {
+//     this.setState({
+//       inputQuery: e.target.value.toLowerCase(),
+//     });
+//   };
+
+//   render() {
+//     const {
+//       currentPage: { page, totalPage },
+//     } = this.props;
+//     return (
+//       <SearchBarHeader>
+//         <SearchForm onSubmit={this.handleSubmit}>
+//           <SearchFormBtn type="submit">
+//             <AiOutlineSearch />
+//           </SearchFormBtn>
+//           <SearchFormInput
+//             type="text"
+//             name="searchQuery"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={this.handleInput}
+//             value={this.state.inputQuery}
+//           />
+//         </SearchForm>
+
+//         {totalPage > 1 && (
+//           <CurrentPage>
+//             {page}/{totalPage}
+//           </CurrentPage>
+//         )}
+//       </SearchBarHeader>
+//     );
+//   }
+// }
